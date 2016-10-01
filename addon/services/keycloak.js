27d6185514cadd28bca2783692a64f223791e8f6
Lucas Holmquist <lucas.holmquist@gmail.com>
2016-09-30 20:27:13 -0400

    must call keycloakInit first, but it has options

--- addon/services/keycloak.js
+++ addon/services/keycloak.js
@@ -1,24 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
-    _kcInit () {
-        return new Ember.RSVP.Promise((resolve, reject) => {
-
-            this.keycloak.init().success(() => {
-                console.log('success on init');
-
-                this.keycloak.loadUserProfile();
-                return resolve(this.keycloak);
-            }).error((err) => {
-                console.log('error on init', err);
-                return reject();
-            });
-        });
-    },
     init () {
         this._super(...arguments);
-
-        this.keycloak = new Keycloak('/keycloak.json');
+        this.keycloak = null;
+    },
+    // Function to setup Keycloak and init it
+    keycloakInit (options) {
+        this.keycloak = new Keycloak(options);
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            this.keycloak.init().success(() => {
+                resolve(this.keycloak);
+            }).error((err) => {
+                console.log('Keycloak init failed', err);
+                reject(err);
+            });
+        });
     },
     loadUserProfile () {
         return new Ember.RSVP.Promise((resolve, reject) => {
@@ -31,36 +28,16 @@ export default Ember.Service.extend({
     },
     login () {
         return new Ember.RSVP.Promise((resolve, reject) => {
-            // Keycloak has already been initialized
-            if (this.keycloak.clientId) {
-                if (this.keycloak.authenticated) {
-                    // They are logged in
-                    // Resolve,  maybe shou
-                    return resolve(this.keycloak);
-                }
-
-                // They are not loggged in after init, so do the login
-                this.keycloak.login();
-                return;
-            } else {
-                // Keycloak has not yet been initalized, so we need to call the init function
-                this.keycloak.init().success(() => {
-                    // Keycloak is now initialized
-                    // We check to see if they are authenticated since this could be from a redirect
-                    if (this.keycloak.authenticated) {
-                        // They are logged in
-                        // Resolve,  maybe should check the token?
-                        return resolve(this.keycloak);
-                    }
-
-                    // They are not loggged in after init, so do the login
-                    this.keycloak.login();
-                    return;
-                }).error((err) => {
-                    console.log('Keycloak init failed', err);
-                    return reject(err);
-                });
+            // Keycloak has already been initialized.
+            if (this.keycloak.authenticated) {
+                // They are logged in
+                // Resolve,  maybe should check the token status eventually
+                return resolve(this.keycloak);
             }
+
+            // They are not loggged in after init, so do the login
+            this.keycloak.login();
+            return reject();
         });
     }
 });
